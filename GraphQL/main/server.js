07a45ffbaa054c3a0b42f038e13d6e29ccdd7350
Marine Mekhakyan <marinemekhakyan@gmail.com>
2021-03-23 20:54:05 -0700

    build server and test query, add dependencies

---
+++ GraphQL/main/server.js
@@ -0,0 +1,20 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type Query {
+        greeting: String
+    }
+`;
+
+ const resolvers = {
+     Query: {
+         greeting: () => 'Hello GraphQL World!'
+     }
+ };
+
+
+const server = new ApolloServer({typeDefs, resolvers});
+server.listen({port: 9001})
+.then(({url}) => console.log(`Server running at ${url}`));
+
+
